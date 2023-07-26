2c765c96c8381232d2ae7cedb39b1d512e7100ff
Manzi12 <manzijosephis@gmail.com>
2023-07-26 10:45:04 +0100

    added customer class to the app

---
+++ src/Customer.java
@@ -0,0 +1,23 @@
+public class Customer {
+    private String name;
+    private String contactNumber;
+
+    public Customer(String name, String contactNumber){
+        this.name = name;
+        this.contactNumber = contactNumber;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getContactNumber(){
+        return contactNumber;
+    }
+
+    public String toString(){
+        return "Customer " +
+                "Name " + name +
+                "Contact Number : " + contactNumber;
+    }
+}
